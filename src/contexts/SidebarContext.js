5bdac976366dbf587e83f891f6bdbf3e8313c830
Daniel Joshua Anyinu <anyinu.d@gmail.com>
2023-08-09 07:16:07 +0100

    initial

--- src/contexts/SidebarContext.js
+++ src/contexts/SidebarContext.js
@@ -1,7 +1,14 @@
-import React from 'react';
+import { useState, createContext } from 'react';
+// create context
+export const SidebarContext = createContext()
+const SidebarProvider = ({ children }) => {
+  // Sidebar State
+  const [isOpened, setIsOpened] = useState(false)
 
-const SidebarContext = () => {
-  return <div>SidebarContext</div>;
+  const handleOpen = () => {
+    setIsOpened(false)
+  }
+  return <SidebarContext.Provider value={{ isOpened, setIsOpened, handleOpen }}>{children}</SidebarContext.Provider>;
 };
 
-export default SidebarContext;
+export default SidebarProvider;
