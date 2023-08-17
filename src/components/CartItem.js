78b14991e782b15a972a5ca9c806968c82dfcdd6
Daniel Joshua Anyinu <anyinu.d@gmail.com>
2023-08-17 10:57:26 +0100

    cart item edits

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -1,5 +1,8 @@
 import React from 'react';
+// import link
 import { Link } from 'react-router-dom';
+// import icons
+import { IoMdAdd, IoMdClose, IoMdRemove } from 'react-icons/io'
 
 const CartItem = ({ item }) => {
   // destructure item
@@ -10,12 +13,37 @@ const CartItem = ({ item }) => {
       <Link to={`/product/${id}`}>
         <img className='max-w-[90px]' src={image} alt='' />
       </Link>
-      <div>
+      <div className='w-full flex flex-col'>
         {/* title & remove icon */}
-        <div>
-          <Link to={`/product/${id}`}>{title}</Link>
+        <div className='flex justify-between mb-2'>
+          {/* title */}
+          <Link to={`/product/${id}`} className='text-sm uppercase font-medium max-w-[240px] text-primary hover:underline'>{title}</Link>
+          {/* remove icon */}
+          <div className='text-xl cursor-pointer'>
+            <IoMdClose className='text-gray-500 hover:text-red-500 transition' />
+          </div>
+        </div>
+        <div className='bg-orange-100 flex gap-x-2 h-[36px] text-sm'>
+          {/* Qty */}
+          <div className='flex flex-1 max-w-[100px]  items-center h-full border text-primary font-medium'>
+            {/* minus icon */}
+            <div className='flex-1 flex justify-center items-center cursor-pointer'>
+              <IoMdRemove />
+            </div>
+            {/* amount */}
+            <div className='h-full flex justify-center items-center px-2'>{amount}</div>
+            {/* plus icon */}
+            <div className='flex-1 h-full flex justify-center items-center cursor-pointer'>
+              <IoMdAdd />
+            </div>
+          </div>
+          {/* Item price */}
+          <div>item price</div>
+          {/* Final price */}
+          <div>final price</div>
         </div>
       </div>
+
     </div>
   </div>;
 };
